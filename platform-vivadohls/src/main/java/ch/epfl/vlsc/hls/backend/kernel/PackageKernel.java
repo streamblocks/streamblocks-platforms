de52d9a0dc3dae670705d2ef498256e12e7ee667
Endri Bezati <endri.bezati@gmail.com>
2019-08-08 13:01:15 +0200

    (platform-vivadohls) Starting Package kernel.

---
+++ platform-vivadohls/src/main/java/ch/epfl/vlsc/hls/backend/kernel/PackageKernel.java
@@ -0,0 +1,38 @@
+package ch.epfl.vlsc.hls.backend.kernel;
+
+import ch.epfl.vlsc.hls.backend.VivadoHLSBackend;
+import ch.epfl.vlsc.platformutils.Emitter;
+import ch.epfl.vlsc.platformutils.PathUtils;
+import org.multij.Binding;
+import org.multij.BindingKind;
+import org.multij.Module;
+import se.lth.cs.tycho.ir.network.Network;
+
+@Module
+public interface PackageKernel {
+
+    @Binding(BindingKind.INJECTED)
+    VivadoHLSBackend backend();
+
+    default Emitter emitter() {
+        return backend().emitter();
+    }
+
+    default void getPackageKernel(){
+        // -- Identifier
+        String identifier = backend().task().getIdentifier().getLast().toString();
+
+        // -- Network
+        Network network = backend().task().getNetwork();
+
+        // -- Network file
+        emitter().open(PathUtils.getTargetScript(backend().context()).resolve("package_kernel.tcl.in"));
+
+
+
+
+        emitter().close();
+    }
+
+
+}
