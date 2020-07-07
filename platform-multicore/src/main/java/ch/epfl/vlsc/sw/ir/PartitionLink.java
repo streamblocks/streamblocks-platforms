fa921769cfd146544777d83b91414757a359ddc2
Endri Bezati <endri.bezati@gmail.com>
2020-07-07 15:48:45 +0200

    (platforms-multicore) implementing withTypeParameters, withValueParameters as per Entity change.

--- platform-multicore/src/main/java/ch/epfl/vlsc/sw/ir/PartitionLink.java
+++ platform-multicore/src/main/java/ch/epfl/vlsc/sw/ir/PartitionLink.java
@@ -1,6 +1,8 @@
 package ch.epfl.vlsc.sw.ir;
 
 
+import se.lth.cs.tycho.ir.decl.ParameterTypeDecl;
+import se.lth.cs.tycho.ir.decl.ParameterVarDecl;
 import se.lth.cs.tycho.ir.util.ImmutableList;
 import se.lth.cs.tycho.ir.entity.Entity;
 import se.lth.cs.tycho.ir.entity.PortDecl;
@@ -33,5 +35,14 @@ public class PartitionLink extends Entity {
         return this.handle;
     }
 
+    @Override
+    public PartitionLink withTypeParameters(List<ParameterTypeDecl> typeParameters) {
+        return new PartitionLink(inputPorts, outputPorts, handle);
+    }
+
+    @Override
+    public PartitionLink withValueParameters(List<ParameterVarDecl> valueParameters) {
+        return new PartitionLink(inputPorts, outputPorts, handle);
+    }
 }
 
