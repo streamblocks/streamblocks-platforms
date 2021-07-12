6243789cd5522a937d1a31a38085537feb0f4537
Endri Bezati <endri.bezati@gmail.com>
2021-07-12 16:54:42 +0200

    (platform-core) Fix due to the change of the CalActor constructor on tycho.

--- platform-core/src/main/java/ch/epfl/vlsc/phases/AddFanoutPhase.java
+++ platform-core/src/main/java/ch/epfl/vlsc/phases/AddFanoutPhase.java
@@ -360,7 +360,7 @@ public class AddFanoutPhase implements Phase {
                 ImmutableList.empty(),
                 ImmutableList.empty(),
                 ImmutableList.empty(),
-                null,
+                null, null,
                 process,
                 ImmutableList.empty(),
                 ImmutableList.empty());
@@ -447,7 +447,7 @@ public class AddFanoutPhase implements Phase {
                 actions.build(),
                 ImmutableList.empty(), // TODO: is this correct?
                 null,
-                null,
+                null, null,
                 ImmutableList.empty(),
                 ImmutableList.empty());
         return fanout;
