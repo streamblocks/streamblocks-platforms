3dd9f24df563c627ead0b6b3aad4a7f6d48b6c0e
Endri Bezati <endri.bezati@gmail.com>
2025-10-06 22:42:45 +0200

    (mlir) Add remove schedule phase, replaces the fsm with a state variable, guard conditions on state, and state update inside the action. Removing AM phases as there is no need for it.

--- platform-mlir/src/main/java/se/lth/cs/mlir/platform/Mlir.java
+++ platform-mlir/src/main/java/se/lth/cs/mlir/platform/Mlir.java
@@ -1,14 +1,27 @@
 package se.lth.cs.mlir.platform;
 
+import java.util.List;
+
+import ch.epfl.vlsc.phases.CommonPhases;
+import ch.epfl.vlsc.phases.RemoveSchedulePhase;
 import se.lth.cs.mlir.phase.MlirPhase;
-import ch.epfl.vlsc.phases.*;
 import se.lth.cs.tycho.compiler.Compiler;
 import se.lth.cs.tycho.ir.util.ImmutableList;
+import se.lth.cs.tycho.phase.ActionCaseToActionsPhase;
+import se.lth.cs.tycho.phase.AddMatchGuardsPhase;
+import se.lth.cs.tycho.phase.AddPrioritiesPhase;
+import se.lth.cs.tycho.phase.AddSchedulePhase;
+import se.lth.cs.tycho.phase.LiftProcessVarDeclsPhase;
+import se.lth.cs.tycho.phase.MergeManyGuardsPhase;
 import se.lth.cs.tycho.phase.Phase;
+import se.lth.cs.tycho.phase.ProcessToCalPhase;
+import se.lth.cs.tycho.phase.RemoveIdleMatchExpressionsPhase;
 import se.lth.cs.tycho.phase.RemoveUnusedEntityDeclsPhase;
+import se.lth.cs.tycho.phase.RenameActorVariablesPhase;
+import se.lth.cs.tycho.phase.ScheduleInitializersPhase;
+import se.lth.cs.tycho.phase.ScheduleUntaggedPhase;
+import se.lth.cs.tycho.phase.SubstitutePatternBindingsPhase;
 import se.lth.cs.tycho.platform.Platform;
-
-import java.util.List;
 
 public class Mlir implements Platform {
 
@@ -22,13 +35,31 @@ public class Mlir implements Platform {
         return "StreamBlocks code-generator for Mlir runtime.";
     }
 
+public static List<Phase> schedulePhases() {
+        return ImmutableList.of(
+                new ActionCaseToActionsPhase(),
+                new RenameActorVariablesPhase(),
+                new LiftProcessVarDeclsPhase(),
+                new ProcessToCalPhase(),
+                new AddSchedulePhase(),
+                new ScheduleUntaggedPhase(),
+                new ScheduleInitializersPhase(),
+                new AddPrioritiesPhase(),
+                new RemoveIdleMatchExpressionsPhase(),
+                new SubstitutePatternBindingsPhase(),
+                new AddMatchGuardsPhase(),
+                new MergeManyGuardsPhase(),
+                new RemoveSchedulePhase()
+        );
+    }
+
     private static final List<Phase> phases = ImmutableList.<Phase>builder()
             .addAll(Compiler.frontendPhases())
             .addAll(Compiler.templatePhases())
             .addAll(CommonPhases.portEnumerationPhases)
             .addAll(Compiler.networkElaborationPhases())
             .addAll(Compiler.nameAndTypeAnalysis())
-            .addAll(Compiler.actorMachinePhases())
+            .addAll(schedulePhases())
             .add(new RemoveUnusedEntityDeclsPhase())
             .add(new MlirPhase())
             .build();
